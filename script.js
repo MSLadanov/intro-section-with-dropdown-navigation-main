06a34f2e73c09311ef057c5caf5511ac57940631
iMac <msladanov@gmail.com>
2023-04-07 20:23:58 +0300

    Desktop menu is ready

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 function toggleMenu(e) {
-  let elem = e.target;
-  elem.nextElementSibling.classList.toggle("menu-active");
-  elem
-    .querySelectorAll(".arrow")
-    .forEach((item) => item.classList.toggle("hidden"));
+  if (!document.querySelectorAll(".menu-active").length) {
+    if (e.target.className === "menu-btn-head") {
+      let elem = e.target;
+      elem.nextElementSibling.classList.toggle("menu-active");
+      elem
+        .querySelectorAll(".arrow")
+        .forEach((item) => item.classList.toggle("hidden"));
+    }
+  } else {
+    document.querySelectorAll(".menu-active").forEach((item) => {
+      if (item.classList.contains("menu-active")) {
+        item.classList.toggle("menu-active");
+        e.target
+          .querySelectorAll(".arrow")
+          .forEach((item) => item.classList.toggle("hidden"));
+      }
+    });
+  }
+}
+
+function toggleOpenedMenu(e) {
+  e.target.parentElement.addEventListener("mouseleave", (event) => {
+    toggleMenu(event);
+  });
 }
 
 document
   .querySelectorAll(".menu-btn-head")
-  .forEach((item) => item.addEventListener("click", (e) => toggleMenu(e)));
+  .forEach((item) => item.addEventListener("mouseover", (e) => toggleMenu(e)));
+document.querySelectorAll(".menu-btn-head").forEach((item) =>
+  item.addEventListener("mouseout", (e) => {
+    toggleOpenedMenu(e);
+  })
+);
